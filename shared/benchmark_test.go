ba40efebab4829c3af3eeecd753447a9e9e45b1b
savioruz <kheril.dev@gmail.com>
2026-02-21 12:05:58 +0700

    feat: update Go version to 1.26 and add benchmark tests for shared functions

---
+++ shared/benchmark_test.go
@@ -0,0 +1,77 @@
+package shared_test
+
+import (
+	"testing"
+
+	"oil/shared"
+)
+
+func BenchmarkConvertStringToBool(b *testing.B) {
+	tests := []string{"true", "false", "1", "0", "True", "False", ""}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _, tt := range tests {
+			_ = shared.ConvertStringToBool(tt)
+		}
+	}
+}
+
+func BenchmarkCalculateTotalPage(b *testing.B) {
+	tests := []struct {
+		total int
+		limit int
+	}{
+		{0, 10},
+		{1, 10},
+		{100, 10},
+		{1000, 10},
+		{10000, 10},
+		{100, 20},
+		{100, 50},
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _, tt := range tests {
+			_ = shared.CalculateTotalPage(tt.total, tt.limit)
+		}
+	}
+}
+
+func BenchmarkFilterByID(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = shared.FilterByID("123", "id", "users")
+	}
+}
+
+func BenchmarkBuildCacheKey(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = shared.BuildCacheKey("user", "123", "profile")
+	}
+}
+
+func BenchmarkBuildCacheKeyWithoutPostfix(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = shared.BuildCacheKey("user")
+	}
+}
+
+func BenchmarkGenerateUniqueFilename(b *testing.B) {
+	filenames := []string{
+		"image.jpg",
+		"document.pdf",
+		"very_long_filename_that_should_be_handled_properly.png",
+		"file.txt",
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _, name := range filenames {
+			_ = shared.GenerateUniqueFilename(name)
+		}
+	}
+}
